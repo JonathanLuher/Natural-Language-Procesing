3c52fa88ea117a2e10f34337408ca16fd27d6e54
JonathanLuher <129464379+JonathanLuher@users.noreply.github.com>
2025-02-27 10:06:51 -0600

    Optimization of code
    In this commit I modularized the functions to reorder the list of names in different cases

--- practica1/practica1_PLN.py
+++ practica1/practica1_PLN.py
@@ -2,29 +2,39 @@ import re
 import tkinter as tk
 from tkinter import filedialog
 
+
+#funciones para formatear y reordenar listas de nombres
+def procesar_nombres(texto):
+    texto = re.sub(r'\n+', ' ', texto)
+    nombres_lista = [name.strip() for name in texto.split('and')]
+    nombres_reordenados = []
+    for name in nombres_lista:
+        partes = name.split(',')
+        if len(partes) == 2:
+            nombres_reordenados.append(f"{partes[1].strip()} {partes[0].strip()}")
+        else:
+            nombres_reordenados.append(name)
+    return ", ".join(nombres_reordenados)
+
+def extraer_nombres(expresion, contenido):
+    resultados = re.findall(expresion, contenido, re.IGNORECASE)
+    return [procesar_nombres(resultado) for resultado in resultados]
+
+#Funcion que busca la lista de autores
 def extraer_autores(contenido):
-    bibtex_authors = re.findall(r'author\s*=\s*[{"]([\s\S]*?)[}"]', contenido, re.IGNORECASE)
-    authors = [re.sub(r'\n+', ' ', author) for author in bibtex_authors]
-    # Reordenar los autores y reemplazar 'and' por comas
-    formatted_authors = []
-    for author in authors:
-        author_list = [name.strip() for name in author.split('and')]
-        #Función para reordenar los nombres
-        reordered_authors = []
-        for name in author_list:
-            names = name.split(',') 
-            if len(names) == 2:
-                reordered_authors.append(f"{names[1].strip()} {names[0].strip()}")
-            else:
-                reordered_authors.append(name)
-        formatted_authors.append(", ".join(reordered_authors))
-    return formatted_authors
+    return extraer_nombres(r'author\s*=\s*[{"]([\s\S]*?)[}"]', contenido)
 
+def extraer_editores(contenido):
+    return extraer_nombres(r'editor\s*=\s*[{"]([\s\S]*?)[}"]', contenido)
 
+#Extraer contenido del titulo
 def extraer_titulo(contenido):
     bibtex_titles = re.findall(r'(?<!book)title\s*=\s*[{"]([\s\S]*?)[}"]', contenido, re.IGNORECASE)
     titles = [re.sub(r'\n+', ' ', title).strip().replace('\xa0', ' ') for title in bibtex_titles]
     return titles
+
+
+#Función para leer el documento
 
 def open_file():
     root = tk.Tk()
@@ -34,10 +44,15 @@ def open_file():
     if file_path:
         with open(file_path, "r", encoding="utf-8") as file:
             contenido = file.read()
+
+            title = extraer_titulo(contenido)
             authors = extraer_autores(contenido)
-            print(authors)
-            title=extraer_titulo(contenido)
-            print(title)
+            editors = extraer_editores(contenido)
+            
+            print("Titulo del articulo:", title)
+            print("Lista de autores:", authors)
+            print("Lista de editores:", editors)
+            
 
 if __name__ == "__main__":
     open_file()
