38d3531fd647266741970f05765949f7ca351e26
Jonathan Luciano <129464379+JonathanLuher@users.noreply.github.com>
2025-02-26 21:33:06 -0600

    ADD extract author from the bib and ris documents
    In this commit I uploaded the first version of the practice 1 of Natural language proccesing where we have to extract information

---
+++ practica1/practica1_PLN.py
@@ -0,0 +1,36 @@
+import re
+import tkinter as tk
+from tkinter import filedialog
+
+def extraer_autores(contenido):
+    bibtex_authors = re.findall(r'author\s*=\s*[{"]([\s\S]*?)[}"]', contenido, re.IGNORECASE)
+    authors = [re.sub(r'\n+', ' ', author) for author in bibtex_authors]
+    # Reordenar los autores y reemplazar 'and' por comas
+    formatted_authors = []
+    for author in authors:
+        author_list = [name.strip() for name in author.split('and')]
+        #Función para reordenar los nombres
+        reordered_authors = []
+        for name in author_list:
+            names = name.split(',') 
+            if len(names) == 2:
+                reordered_authors.append(f"{names[1].strip()} {names[0].strip()}")
+            else:
+                reordered_authors.append(name)
+        formatted_authors.append(", ".join(reordered_authors))
+    return formatted_authors
+
+
+def open_file():
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename(title="Selecciona el archivo", filetypes=[("BibTeX and RIS files", "*.bib;*.ris"), ("All files", "*.*")])
+    
+    if file_path:
+        with open(file_path, "r", encoding="utf-8") as file:
+            contenido = file.read()
+            authors = extraer_autores(contenido)
+            print(authors)
+
+if __name__ == "__main__":
+    open_file()
