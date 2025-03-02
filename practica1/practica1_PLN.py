85449c243b09cbb0d6d16f70fca3d610faa2c228
JonathanLuher <129464379+JonathanLuher@users.noreply.github.com>
2025-03-01 23:23:43 -0600

    Regular expresions
    I added new regular expressions to get data

--- practica1/practica1_PLN.py
+++ practica1/practica1_PLN.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import filedialog
 import GetData_bibtext
 
+data=["Autores: ","Editores: ", "Titulo: ", "Libro: ", "Año de publicacion: ","Mes de publicacion: ","Dia de publicacion: ","Editorial: ", "Direccion: ","Paginas: ","Abstract: ", "ISBN: ", "Journal: ","ISSN: ", "Doi: ", "url: "]
+
 #Función para leer el documento
 def open_file():
     root = tk.Tk()
@@ -12,19 +14,11 @@ def open_file():
         with open(file_path, "r", encoding="utf-8") as file:
             contenido = file.read()
             datos = GetData_bibtext.extraer_datos_bibtext(contenido)
+
+            for datatype, databib in  zip(data,datos):
+                print(datatype, databib)
             
-            print(
-            "Autores: ",datos[0],
-            "\nEditores: ",datos[1],
-            "\nTitulo: ",datos[2],
-            "\nLibro: ",datos[3],
-            "\nAño de publicacion: ",datos[4],
-            "\nEditorial: ",datos[5],
-            "\nDireccion: ",datos[6],
-            "\nPaginas: ",datos[7],
-            "\nAbstract: ",datos[8],
-            "\nISBN: ",datos[9]
-            )
+
 
 if __name__ == "__main__":
     open_file()
